c95915eb7c2b2d9ceb47823a0cb005f40e7f6807
gihan12345 <gihanrathnayaka076@gmail.com>
2025-03-24 02:54:25 +0530

    my3
    complete user model

--- homestock-backend/models/User.js
+++ homestock-backend/models/User.js
@@ -1,11 +1,43 @@
-import mongoose from "mongoose";
+// models/UserModel.js
+import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
-});
+// Define the User Schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: false,
+      unique: false,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: false,
+      unique: false,
+      lowercase: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+      unique: false,
+    },
+    password: {
+      type: String,
+      required: false,
+      minlength: 4, // Set a minimum password length for security
+    },
+    confirmPassword: {
+      type: String,
+      required: false,
+    },
+    
+  },
+);
 
-const User = mongoose.model("User", userSchema);
+// Create the User Model
+const user = mongoose.model('User', userSchema);
 
-export default User;
+module.exports = user;
+
