2438355788422e25edd43402f885607266e6168c
TharushaPramod <pramodtharusha@gmail.com>
2025-10-27 11:22:24 +0530

    Update Useform.jsx

--- homestock-app/src/components/Inventory_com/Useform.jsx
+++ homestock-app/src/components/Inventory_com/Useform.jsx
@@ -235,7 +235,7 @@ const Useform = ({ addUseItem, submitted, items, useItems, data, updateUseItem, 
 
                     <FormControl fullWidth required>
                         <InputLabel id="useType" size="small" sx={{ color: '#6b7280' }}>
-                            Quantity Type
+                            Quantity Typessssssssssssssssssss
                         </InputLabel>
                         <Select
                             labelId="useType"
